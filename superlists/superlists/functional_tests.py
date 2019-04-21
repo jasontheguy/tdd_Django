2488add1cb209943c1ad983726175c5ca3cfbacc
Jason Renaud <renaudjj@g.cofc.edu>
2019-04-21 12:29:51 -0600

    Added class and methods to functional test

--- superlists/superlists/functional_tests.py
+++ superlists/superlists/functional_tests.py
@@ -1,6 +1,24 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
+class NewVisitorTest(unittest.TestCase):
 
-assert 'Django' in browser.title
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_start_list_and_retrieve_later(self):
+        #Functional tests follow user Stories like so: Susie Q heard about a new to-do app.
+        #She goes to check out it's homepage. 
+        self.browser.get('http://localhost:8000') #Test mimics user
+        
+        #She noticess page title and header mention to-do lists
+        self.assertIn('To-Do', self.browser.title)
+        self.fail('Finish the test')
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
+
+    
